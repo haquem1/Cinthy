d24cbbb30687774e0205982fca6f18d5d16c29ec
haquem1 <maroof.haque@gmail.com>
2017-01-10 12:34:44 -0800

    hours response

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            if (!richMessage(event.sender.id, event.message.text)) {
+            // if (!richMessage(event.sender.id, event.message.text)) {
                 // auto reply to sender
                 sendMessage(event.sender.id, {text: "Thank you for your message! A staff member from the Career Center will get back to you shortly"});
-            }
+            // }
         } else if (event.postback) {
             console.log("Postback received: " + JSON.stringify(event.postback));
         }
