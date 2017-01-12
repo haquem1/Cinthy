c81f89ffc61a03481ce9c7e40af5599b09365758
haquem1 <maroof.haque@gmail.com>
2017-01-12 12:33:39 -0800

    debug for wks and mos options: week

--- index.js
+++ index.js
@@ -113,7 +113,6 @@ function richMessage(recipientId, text) {
                         ];
 
             if (text.indexOf("week") != -1){
-              sendMessage(recipientId, {text: "No events for next week!"});
                 if (text.indexOf("next week") != -1){
                   for (var i = 0; i < ccEvents.length; i++){
                     if (ccEvents[i].tid.getUTCWeek() + 1 == compare.getUTCWeek()){
@@ -138,6 +137,7 @@ function richMessage(recipientId, text) {
                              };
                      }
                     }
+                    sendMessage(recipientId, {text: "No events for next week!"});
                     if (found){
                       sendMessage(recipientId, message);
                       return true;
