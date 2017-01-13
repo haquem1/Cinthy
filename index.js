01f9c1ac1cbe5042316eee0d6d4ed83f79e7c8ff
haquem1 <maroof.haque@gmail.com>
2017-01-13 12:51:18 -0800

    basic search: techfest debug

--- index.js
+++ index.js
@@ -275,8 +275,7 @@ function richMessage(recipientId, text) {
                                 if (++count == 1){
                                   sendMessage(recipientId, {text: "Here are some events to help you:"});
                                 }
-                               sendMessage(recipientId, message);
-                               i = 0;
+                              //  sendMessage(recipientId, message);
                         }
                       }
                     }
