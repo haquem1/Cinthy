9ff2838f09e5292343b0c38ff20abee5a8845a95
haquem1 <maroof.haque@gmail.com>
2017-01-17 23:28:17 -0800

    beginning of modular code

---
+++ modules/template.js
@@ -0,0 +1,9 @@
+function template(keywords, events){
+
+  return message; //will be JSON object
+
+};
+
+// TODO create dictionary with [keywords, name]
+
+// TODO create element array template for catalogue look in message template
