9df4cf569ea8ec3467671bdbcaecc8bf12f29b5d
haquem1 <maroof.haque@gmail.com>
2017-03-18 03:27:13 -0700

    rollback

--- index.js
+++ index.js
@@ -85,9 +85,12 @@ function richMessage(recipientId, text) {
     values = text.split(' ');
 
     // message for staff
-    for (var i = 0; i < keys.message.length; i++) {
-       if (values[0] == keys.message[i]) return false;
-    }
+    if (keys.message.indexOf(values[0]) != -1) return false;
+
+
+    // for (var i = 0; i < keys.message.length; i++) {
+    //    if (values[0] == keys.message[i]) return false;
+    // }
 
     // get started
     for (var i = 0; i < keys.help.length; i++) {
