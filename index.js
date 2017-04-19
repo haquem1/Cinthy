bc26a35d5d897973fe82466ae81c618893d1c885
haquem1 <maroof.haque@gmail.com>
2017-04-19 14:28:39 -0700

    adding location, phone number, and sunlink pass capabilities

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function richMessage(recipientId, text) {
           }
         }
         if (sendMessage(recipientId, message)){
-        message = "Hi! This is Cinthy the Career Center Assistant.\n\nYou can ask me about:\n-Our hours\n-Our upcoming events for this month, next month, and the semester\n\nI can also recommend events for you. Or, if you know which event you're looking for, just ask!\n\nType 'message' followed by your message if there's something you would like our staff to answer.\n\nSimply say hello or help to bring this screen up again!\n\n\nVisit us at http://csun.edu/career or call us 818-677-2878\nWe are located on the 4th floor of Bayramian Hall"
+        message = "Hi! This is Cinthy the Career Center Assistant.\n\nYou can ask me about:\n-Our hours\n-Our upcoming events for this month, next month, and the semester\n\nI can also recommend events for you. Or, if you know which event you're looking for, just ask!\n\nType 'message' followed by your message if there's something you would like our staff to answer.\n\nSimply say hello or help to bring this screen up again!\n\n\nVisit us at http://www.csun.edu/career or call us 818-677-2878\nWe are located on the 4th floor of Bayramian Hall"
         sendMessage(recipientId, {
             text: message
         });
@@ -108,7 +108,7 @@ function richMessage(recipientId, text) {
     // sunlink block
     for (var i = 0; i < keys.sunlink.length; i++) {
         if (text.indexOf(keys.sunlink[i]) != -1) {
-                message = "If you are a current student, your Sunlink password is #CareerCenter for your first log in.\n\nPlease contact the Career Center if you are not a current student or the password is not working.\n\nVisit us at http://csun.edu/career or call us 818-677-2878\nWe are located on the 4th floor of Bayramian Hall";
+                message = "If you are a current student, your Sunlink password is #CareerCenter for your first log in.\n\nPlease contact the Career Center if you are not a current student or the password is not working.\n\nVisit us at http://www.csun.edu/career or call us 818-677-2878\nWe are located on the 4th floor of Bayramian Hall";
             sendMessage(recipientId, {
                 text: message
             });
