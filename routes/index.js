378035b2fc6c6e76a6f7db96625e2cd60103dab3
haquem1 <maroof.haque@gmail.com>
2017-08-01 12:47:48 -0700

    debug

--- routes/index.js
+++ routes/index.js
@@ -23,11 +23,11 @@ routes.post('/webhook', function(req, res) {
     for (var i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            if (!richMessage(event.sender.id, event.message.text)) {
+           // if (!richMessage(event.sender.id, event.message.text)) {
                 sendMessage(event.sender.id, {
                     text: "Thank you for your message! A staff member from the Career Center will get back to you shortly\n\nVisit us at Bayramian Hall, 413 and at www.csun.edu/career"
                 });
-            }
+           // }
         } else if (event.postback) {
             console.log("Postback received: " + JSON.stringify(event.postback));
         }
