1a074c6d1a53afeaeb75db06fb7896825886e538
haquem1 <maroof.haque@gmail.com>
2017-08-08 10:18:28 -0700

    adding async calls

--- helpers/richMessage/index.js
+++ helpers/richMessage/index.js
@@ -30,7 +30,7 @@ richMessage = function (recipientId, message) {
     if (keys.message.indexOf(values[0]) != -1) return false;
 
     // greeting
-    if (keys.help.indexOf(values[0]) != -1 && values.length < 12) {
+    if (keys.help.indexOf(values[0]) != -1 && values.length < 18) {
         message = "Hi! This is Cinthy the Career Center Assistant.\n\nHow may I help you?";
         fiber(function() {
             await( sendMessage(recipientId, { text: message }) );
@@ -43,7 +43,7 @@ richMessage = function (recipientId, message) {
       }
 
     // get started
-    if (keys.get_started.indexOf(values[0]) != -1 && values.length < 12) {
+    if (keys.get_started.indexOf(values[0]) != -1 && values.length < 24) {
         message = "You ask me about:\n-Our hours\n-Our upcoming events for this month, next month, and the semester\n\nI can also recommend events for you. Or, if you know which event you're looking for, just ask!\n\nType 'message' followed by your message if there's something you would like our staff to answer.\n\nSimply say hello or help to bring this screen up again!\n\n\nVisit us at http://www.csun.edu/career or call us 818-677-2878\nWe are located on the 4th floor of Bayramian Hall";
         sendMessage(recipientId, {
             text: message
